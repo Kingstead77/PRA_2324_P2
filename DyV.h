1207e6a57b4651aab9976d6bd6425b14b16a0c37
Kingstead77 <mmarcar2@etsinf.upv.es>
2024-12-04 14:10:01 +0100

    añadida version definitiva

--- DyV.h
+++ DyV.h
@@ -1,40 +1,88 @@
-
-
-#ifndef DYV_H
-#define DYV_H
-
 #include <vector>
-
-// float epsilon = 1e-6;
+#include <iostream>
 
 template <typename T>
-int busquedaBinaria(T x, std::vector<T> &v, int ini, int fin){
-
-        if (ini > fin)
-                return -1;      //no se ha encontrado el valor
-        int mitad = ((ini+fin)/2);
-
-        if (v[mitad] == x)
-                return mitad;
-        else if (x > v[mitad])
-                return busquedaBinaria(x, v, mitad + 1, fin);
-        else
-                return busquedaBinaria(x, v, ini, mitad - 1);
-
+int BusquedaBinaria(T x, std::vector<T> &v, int ini, int fin){
+	if(ini > fin){
+		return -1;
+	}
+	int medio=(ini + fin ) / 2;
+	if(v[medio] == x){
+		return medio;
+	}
+	else if(v[medio] > x){
+		return BusquedaBinaria(x ,v ,ini ,medio -1);
+	}
+	else{
+		return BusquedaBinaria(x, v, medio+1, fin);
+	}
 }
 
-int busquedaBinariaInv(int x, std::vector<int> &v, int ini, int fin){
-
-        if (ini > fin)  
-                return -1;      
-        int mitad = ((ini+fin)/2);
-
-        if (v[mitad] == x)
-                return mitad;
-        else if (x > v[mitad])
-                return busquedaBinariaInv(x, v, ini, mitad-1); //se cambian los margenes
-        else
-                return busquedaBinariaInv(x, v, mitad+1, fin);
-
+template <typename T>
+int BusquedaBinariaInv(T x, std::vector<T> &v, int ini, int fin){
+	if(ini > fin){
+		return -1;
+	}
+	int medio=(ini+fin)/2;
+	if(v[medio] == x){
+		return medio;
+	}
+	else if(v[medio] < x){
+		return BusquedaBinariaInv(x, v, ini, medio -1);
+	}
+	else{
+		return BusquedaBinariaInv(x, v, medio +1, fin);
+	}
 }
-#endif 
+
+template <typename T>
+int Partition(std::vector<T> &v, int ini, int fin){
+	T pivote = v[fin];
+	int i = ini;
+	for (int j = ini ; j < fin ; j++){
+		if( v[j] <= pivote){
+			T aux = v[j];
+			v[j] = v[i];
+			v[i] = aux;
+			i++;
+		}
+	}
+	T aux = v[i];
+	v[i] =v [fin];
+	v[fin] = aux;
+	return i;
+}
+
+template <typename T>
+int PartitionFirst(std::vector<T> &v, int ini, int fin) {
+    T pivote = v[ini]; 
+    int i = ini + 1;   
+
+    for (int j = ini + 1; j <= fin; j++) { 
+        if (v[j] <= pivote) {
+            std::swap(v[j], v[i]); 
+            i++;
+        }
+    }
+
+    std::swap(v[ini], v[i - 1]);
+    return i - 1; 
+}
+
+
+template <typename T>
+void quickSort(std::vector<T> &v, int ini, int fin, int op){
+	//std::cout<<"ini: "<<ini<<", fin: "<<fin<<std::endl;
+	if(ini < fin){
+		int pivot;
+		switch (op){
+			case 1:
+				pivot = Partition(v, ini, fin);
+				break;
+			case 2:
+				pivot = PartitionFirst(v, ini, fin);
+		}
+		quickSort(v, ini, pivot-1, op);
+		quickSort(v, pivot+1, fin, op);
+	}
+}
--- test_DyV.cpp
+++ test_DyV.cpp
@@ -1,28 +1,53 @@
 #include <iostream>
 #include <vector>
 #include "DyV.h"
-
+#include <chrono>
+#define size 10
 
 using namespace std;
 
 int main(){
-
-	vector<int> vecInt{1,2,3,4,5,6,7,8,9,10};
-	vector<double> vecFloat{1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9, 10.1}; //al usar floats hay que tener en cuenta tolerancias al comprar valores, si no no funciona
-
-	vector<int> vecIntInv{10,9,8,7,6,5,4,3,2,1};
-
-	int size_vecInt= vecInt.size();
-	int size_vecFloat = vecFloat.size();
-	int size_vecIntInv = vecIntInv.size();
-
-	int resInt= busquedaBinaria(4, vecInt, 0, size_vecInt-1);
-	int resFloat = busquedaBinaria(6.6, vecFloat, 0, size_vecFloat-1);
-	int resIntInv = busquedaBinariaInv(5, vecIntInv, 0, size_vecIntInv-1);
+	/*
+	vector<int> intVector{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	vector<double> floatVector{0.1f, 2.3f, 4.5f, 6.7f, 8.9f, 10.11, 12.3, };
+	vector<char> charVector{'a','b','c','d','e','f','g','h','i'};
 	
-	cout << resInt << endl;
-	cout << resFloat << endl;
-	cout << resIntInv << endl;
+	cout<<BusquedaBinaria(3, intVector, 0, size -1) << endl;
+	cout<<BusquedaBinaria(0.1, floatVector, 0, size -1) << endl;
+	cout<<BusquedaBinaria('i', charVector, 0, size -1) << endl;
+	
+	vector<int> intVectorInv{7,6,5,4,3,2,1};
+	vector<double> floatVectorInv{9.8f,7.6f,5.4f,3.2f,1.0f};
+	vector<char> charVectorInv{'i','h','g','f','e','d','c','b','a'};
+	
+	cout<<BusquedaBinariaInv(3, intVectorInv, 0, size -1) << endl;
+	cout<<BusquedaBinariaInv(1.0, floatVectorInv, 0, size -1) << endl;
+	cout<<BusquedaBinariaInv('i', charVectorInv, 0, size -1) << endl;
+	*/
+    
+	vector<int> intDesorden{1,8,2,4,6,7,5};
+	vector<double> floatDesorden{1.2f,9.5f,6.4f,2.7f,3.0f};
+	vector<char> charDesorden{'f','s','a','e','j','v','b'};
+    /*
+	quickSort(intDesorden, 0, size-1, 1);
+	for(int i=0;i< size;i++){
+		cout<<intDesorden[i]<<", ";
+	}
+	cout<<endl;
+	*/
+    //Prueba de tiempo
+
+    auto start = std::chrono::system_clock::now();
+    quickSort(intDesorden, 0, size - 1, 1);
+    auto end = std::chrono::system_clock::now();
+    std::chrono::duration<float, std::milli> duration = end - start;
+    std::cout << "tiempo con pivote al final: " << duration.count() << " ms" << std::endl;
+
+    start = std::chrono::system_clock::now(); 
+    quickSort(intDesorden, 0, size - 1, 2);
+    end = std::chrono::system_clock::now();   
+    duration = end - start;                   
+    std::cout << "tiempo con pivote al principio: " << duration.count() << " ms" << std::endl;
 
 	return 0;
 }
